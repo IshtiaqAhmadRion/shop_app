e7da0835dc2a579b3db0a0351c93b05658d7c9c4
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-02 02:15:46 +0600

    Update default_button.dart

--- lib/components/default_button.dart
+++ lib/components/default_button.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../size_config.dart';
 
-
 class CustomButton extends StatelessWidget {
   const CustomButton({
-    Key? key, required this.text, required this.press,
+    Key? key,
+    required this.text,
+    required this.voidCallback
   }) : super(key: key);
   final String text;
-  final Function press;
+  final VoidCallback voidCallback;
+  //final Function press;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,7 @@ class CustomButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
         ),
         color: kPrimaryColor,
-        onPressed: ()=>press,
+        onPressed: voidCallback,
         child: Text(
           text,
           style: TextStyle(
