cfc144dcf6168048deef790823855661521fdfc1
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-02 21:08:01 +0600

    click not working

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -10,7 +11,8 @@ class Body extends StatelessWidget {
         child: SizedBox(
       width: double.infinity,
       child: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
         child: Column(
           children: [
             Text(
@@ -48,7 +50,21 @@ class _SignFormState extends State<SignForm> {
         children: [
           TextFormField(
             decoration: InputDecoration(
-              hintText: "Enter Your Email"
+              labelText: "Email",
+              hintText: "Enter Your Email",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+                borderSide: BorderSide(color: kTextColor),
+                gapPadding: 10,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+                borderSide: BorderSide(color: kTextColor),
+                gapPadding: 10,
+              ),
             ),
           )
         ],
