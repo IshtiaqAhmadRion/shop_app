fd906bca68591aa6617ef7cf437720b39ae22419
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-05 02:11:53 +0600

    Update no_account_text.dart

--- lib/components/no_account_text.dart
+++ lib/components/no_account_text.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
+//import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 
 import '../constants.dart';
 import '../size_config.dart';
@@ -19,8 +19,7 @@ class NoAccountText extends StatelessWidget {
           style: TextStyle(fontSize: getProportionateScreenWidth(16)),
         ),
         GestureDetector(
-          onTap:()=>  Navigator.popAndPushNamed(
-            context, ForgotPasswordScreen.routeName),
+          onTap:(){},
           child: Text(
             "Sign Up ",
             style: TextStyle(
