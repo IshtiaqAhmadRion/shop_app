e2dd87c8833c3bc3ce640f28d5f4667e43dc64d4
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-06 02:12:09 +0600

    Update routs.dart

--- lib/routs.dart
+++ lib/routs.dart
@@ -2,7 +2,9 @@
 
 //import 'dart:js';
 
+
 import 'package:flutter/widgets.dart';
+import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
@@ -16,4 +18,5 @@ final Map<String, WidgetBuilder> routes = {
   ForgotPasswordScreen.routeName: (context)=>ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen()
 };
