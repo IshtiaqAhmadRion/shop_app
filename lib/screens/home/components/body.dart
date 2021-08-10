2a5b6013f3a1ecc8ba479460e282fffbd5c64980
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-10 15:42:37 +0600

    search added

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 // import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/size_config.dart';
 
@@ -16,21 +17,43 @@ class Body extends StatelessWidget {
           padding:
               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
             children: [
               Container(
                 width: Sizeconfig.screenWidth! * 0.6,
-                height: 50,
+                // height: 50,
                 decoration: BoxDecoration(
                   color: kSecondaryColor.withOpacity(0.1),
                   borderRadius: BorderRadius.circular(15),
                 ),
                 child: TextField(
+                  onChanged: (value){
+                    //search value
+                  },
                   decoration: InputDecoration(
                     enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none
+                    focusedBorder: InputBorder.none,
+                    hintText: "Search Product",
+                    prefixIcon: Icon(Icons.search),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(20),
+                      vertical: getProportionateScreenWidth(9),
+                    )
+
                   ),
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+                height: getProportionateScreenWidth(46),
+                width: getProportionateScreenWidth(46),
+                decoration: BoxDecoration(
+                  color: kSecondaryColor.withOpacity(0.1),
+                  shape: BoxShape.circle,
+                ),
+                child: SvgPicture.asset("assets/icons/Bell.svg"),
+              )
             ],
           ),
         )
