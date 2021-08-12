feb90f01986ce8d5ca9457c2589358eae9948a51
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-12 14:11:28 +0600

    stack widget

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -11,65 +11,78 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(
-      children: [
-        Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: SingleChildScrollView(
+          child: Column(
+              children: [
+                SizedBox(
+                  height: getProportionateScreenWidth(20),
 
-            children: [
-              Container(
-                width: Sizeconfig.screenWidth! * 0.6,
-                // height: 50,
-                decoration: BoxDecoration(
-                  color: kSecondaryColor.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(15),
                 ),
-                child: TextField(
-                  onChanged: (value){
-                    //search value
-                  },
-                  decoration: InputDecoration(
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    hintText: "Search Product",
-                    prefixIcon: Icon(Icons.search),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: getProportionateScreenWidth(20),
-                      vertical: getProportionateScreenWidth(9),
-                    )
-
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        
+              children: [
+                Container(
+                  width: Sizeconfig.screenWidth! * 0.6,
+                  // height: 50,
+                  decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ),
-              ),
-              Stack(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(getProportionateScreenWidth(12)),
-                    height: getProportionateScreenWidth(46),
-                    width: getProportionateScreenWidth(46),
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor.withOpacity(0.1),
-                      shape: BoxShape.circle,
-                    ),
-                    child: SvgPicture.asset("assets/icons/Bell.svg"),
-                  ),
-                  Container(
-                    height: getProportionateScreenWidth(16),
-                    width: getProportionateScreenWidth(16),
-                    decoration: BoxDecoration(
-                      color: Color(0XFFFF4848),
+                  child: TextField(
+                    onChanged: (value){
+                      //search value
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: "Search Product",
+                      prefixIcon: Icon(Icons.search),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: getProportionateScreenWidth(20),
+                        vertical: getProportionateScreenWidth(9),
+                      )
+        
                     ),
                   ),
-
-                ],
-              )
-            ],
-          ),
-        )
-      ],
-    ));
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+                      height: getProportionateScreenWidth(46),
+                      width: getProportionateScreenWidth(46),
+                      decoration: BoxDecoration(
+                        color: kSecondaryColor.withOpacity(0.1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/Bell.svg"),
+                    ),
+                    Positioned(
+                      top: -3,
+                      right: 0,
+                      child: Container(
+                        height: getProportionateScreenWidth(16),
+                        width: getProportionateScreenWidth(16),
+                        decoration: BoxDecoration(
+                          color: Color(0XFFFF4848),
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 1.5,color: Colors.white),
+                    
+                        ),
+                      ),
+                    ),
+        
+                  ],
+                )
+              ],
+            ),
+          )
+              ],
+            ),
+        ));
   }
 }
