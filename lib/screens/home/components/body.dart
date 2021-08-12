841ee458be454aab62220ab28d0657d15e211957
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-12 17:29:02 +0600

    bell icon and card icon added

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -12,18 +12,16 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-              children: [
-                SizedBox(
-                  height: getProportionateScreenWidth(20),
-
-                ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: getProportionateScreenWidth(20),
+          ),
           Padding(
-            padding:
-                EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20)),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        
               children: [
                 Container(
                   width: Sizeconfig.screenWidth! * 0.6,
@@ -33,56 +31,92 @@ class Body extends StatelessWidget {
                     borderRadius: BorderRadius.circular(15),
                   ),
                   child: TextField(
-                    onChanged: (value){
+                    onChanged: (value) {
                       //search value
                     },
                     decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Search Product",
-                      prefixIcon: Icon(Icons.search),
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: getProportionateScreenWidth(20),
-                        vertical: getProportionateScreenWidth(9),
-                      )
-        
-                    ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Search Product",
+                        prefixIcon: Icon(Icons.search),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20),
+                          vertical: getProportionateScreenWidth(9),
+                        )),
                   ),
                 ),
-                Stack(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(getProportionateScreenWidth(12)),
-                      height: getProportionateScreenWidth(46),
-                      width: getProportionateScreenWidth(46),
-                      decoration: BoxDecoration(
-                        color: kSecondaryColor.withOpacity(0.1),
-                        shape: BoxShape.circle,
-                      ),
-                      child: SvgPicture.asset("assets/icons/Bell.svg"),
-                    ),
-                    Positioned(
-                      top: -3,
-                      right: 0,
-                      child: Container(
-                        height: getProportionateScreenWidth(16),
-                        width: getProportionateScreenWidth(16),
-                        decoration: BoxDecoration(
-                          color: Color(0XFFFF4848),
-                          shape: BoxShape.circle,
-                          border: Border.all(width: 1.5,color: Colors.white),
-                    
-                        ),
-                      ),
-                    ),
-        
-                  ],
-                )
+                IconBtnWithCounter(
+                  svgSrc: "assets/icons/Cart Icon.svg",
+                  numOfItems: 3,
+                  press: (){},
+                ),
+                IconBtnWithCounter(
+                  svgSrc: "assets/icons/Bell.svg",
+                  numOfItems: 3,
+                  press: (){},
+                ),
               ],
             ),
           )
-              ],
+        ],
+      ),
+    ));
+  }
+}
+
+class IconBtnWithCounter extends StatelessWidget {
+  const IconBtnWithCounter({
+    Key? key, required this.svgSrc, this.numOfItems = 0, required this.press,
+  }) : super(key: key);
+
+  final String svgSrc;
+  final int numOfItems;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      borderRadius: BorderRadius.circular(50),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+            height: getProportionateScreenWidth(46),
+            width: getProportionateScreenWidth(46),
+            decoration: BoxDecoration(
+              color: kSecondaryColor.withOpacity(0.1),
+              shape: BoxShape.circle,
             ),
-        ));
+            child: SvgPicture.asset(svgSrc),
+          ),
+          Positioned(
+            top: -3,
+            right: 0,
+            child: Container(
+              height: getProportionateScreenWidth(16),
+              width: getProportionateScreenWidth(16),
+              decoration: BoxDecoration(
+                color: Color(0XFFFF4848),
+                shape: BoxShape.circle,
+                border: Border.all(width: 1.5, color: Colors.white),
+              ),
+              child: Center(
+                child: Text(
+                  "$numOfItems",
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(10),
+                    height: 1,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
