0e6d701c46099cbc468fef3e7e084e421b6ec09d
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-16 23:50:57 +0600

    section title

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -8,6 +8,7 @@ import 'package:shop_app/size_config.dart';
 import 'catagories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
+import 'section_title.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -40,34 +41,5 @@ class Body extends StatelessWidget {
         ],
       ),
     ));
-  }
-}
-
-class SectionTitle extends StatelessWidget {
-  const SectionTitle({
-    Key? key,
-    required this.text,
-    required this.press,
-  }) : super(key: key);
-  final String text;
-  final GestureTapCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            text,
-            style: TextStyle(
-                fontSize: getProportionateScreenWidth(18), color: Colors.black),
-          ),
-          GestureDetector(onTap: press, child: Text("See More")),
-        ],
-      ),
-    );
   }
 }
