07e4c525a65773271da3391b5ddd4732cd7d7227
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-16 14:11:46 +0600

    Home Header

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,10 @@
 // import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
+
 import 'package:shop_app/size_config.dart';
 
-import 'icon_btn_with_counter.dart';
+import 'home_header.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -18,47 +18,17 @@ class Body extends StatelessWidget {
           SizedBox(
             height: getProportionateScreenWidth(20),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: Sizeconfig.screenWidth! * 0.6,
-                  // height: 50,
-                  decoration: BoxDecoration(
-                    color: kSecondaryColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: TextField(
-                    onChanged: (value) {
-                      //search value
-                    },
-                    decoration: InputDecoration(
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: "Search Product",
-                        prefixIcon: Icon(Icons.search),
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: getProportionateScreenWidth(20),
-                          vertical: getProportionateScreenWidth(9),
-                        )),
-                  ),
-                ),
-                IconBtnWithCounter(
-                  svgSrc: "assets/icons/Cart Icon.svg",
-                  numOfItems: 3,
-                  press: (){},
-                ),
-                IconBtnWithCounter(
-                  svgSrc: "assets/icons/Bell.svg",
-                  numOfItems: 3,
-                  press: (){},
-                ),
-              ],
+          HomeHeader(),
+          Container(
+            width: double.infinity,
+            height: 90,
+            decoration: BoxDecoration(
+              color: Color(0xFF4A3298),
             ),
-          )
+          ),
+        
+
+
         ],
       ),
     ));
