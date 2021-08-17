24bf2bde31bd2adaf526ac26467aa7f750aa04b3
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-18 00:48:08 +0600

    Special offer scroller

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,14 +1,12 @@
-// import 'dart:ui';
 
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:shop_app/size_config.dart';
 
 import 'catagories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
-import 'section_title.dart';
+import 'special_offers.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -34,93 +32,9 @@ class Body extends StatelessWidget {
           SizedBox(
             height: getProportionateScreenWidth(30),
           ),
-          SectionTitle(
-            text: "Special for you",
-            press: () {},
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SpecialOfferCard(
-                  image: "assets/images/Image Banner 2.png",
-                  category: "Smartphones",
-                  numOfBrand: 18,
-                  press: (){},
-                ),
-                 SpecialOfferCard(
-                  image: "assets/images/Image Banner 2.png",
-                  category: "Smartphones",
-                  numOfBrand: 18,
-                  press: (){},
-                ),
-              ],
-            ),
-          ),
+          SpecialOffers(),
         ],
       ),
     ));
-  }
-}
-
-class SpecialOfferCard extends StatelessWidget {
-  const SpecialOfferCard({
-    Key? key, required this.category, required this.image, required this.numOfBrand, required this.press,
-  }) : super(key: key);
-
-  final String category, image;
-  final int numOfBrand;
-  final GestureTapCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding:  EdgeInsets.only(left:getProportionateScreenWidth(20)),
-      child: SizedBox(
-        width: getProportionateScreenWidth(242),
-        height: getProportionateScreenWidth(100),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Stack(
-            children: [
-              Image.asset(
-                image,
-                fit: BoxFit.cover,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF343434).withOpacity(0.4),
-                      Color(0xFF343434).withOpacity(0.15)
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(15),
-                    vertical: getProportionateScreenWidth(10)),
-                child: Text.rich(TextSpan(
-                  style: TextStyle(color: Colors.white),
-                  children: [
-                    TextSpan(
-                      text: "$category\n",
-                      style: TextStyle(
-                        fontSize: getProportionateScreenWidth(18),
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextSpan(text: "$numOfBrand Brands"),
-                  ],
-                )),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
