a9e21720cd180257b3c2cb04bbb4d27d4b4ac21c
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-19 00:51:23 +0600

    popular product done

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/constants.dart';
-import 'package:shop_app/models/product.dart';
-import 'package:shop_app/screens/home/components/section_title.dart';
 
 import 'package:shop_app/size_config.dart';
 
 import 'catagories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
+import 'popular_products.dart';
 import 'special_offers.dart';
 
 class Body extends StatelessWidget {
@@ -38,41 +36,9 @@ class Body extends StatelessWidget {
           SizedBox(
             height: getProportionateScreenWidth(30),
           ),
-          SectionTitle(text: "Populer Product", press: () {}),
-          SizedBox(
-            width: getProportionateScreenWidth(140),
-            child: Column(
-              children: [
-                AspectRatio(
-                  aspectRatio: 1.02,
-                  child: Container(
-                    padding: EdgeInsets.all(getProportionateScreenWidth(20)),
-                    decoration: BoxDecoration(
-                      color: kSecondaryColor.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Image.asset(demoProducts[0].images[0]),
-                  ),
-                ),
-                const SizedBox(height: 5,),
-                Text(
-                  demoProducts[0].title,
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                  maxLines: 2,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "\$${demoProducts[0].price}",
-                      style: TextStyle(fontSize: getProportionateScreenWidth(18),fontWeight: FontWeight.w600,color: kPrimaryColor),
-                      
-                    ),
-                  ],
-                )
-              ],
-            ),
+          PopularProducts(),
+           SizedBox(
+            height: getProportionateScreenWidth(30),
           ),
         ],
       ),
---
+++ lib/screens/home/components/popular_products.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/screens/home/components/section_title.dart';
+
+import '../../../size_config.dart';
+import 'product_card.dart';
+
+
+class PopularProducts extends StatelessWidget {
+  const PopularProducts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          text: "Populer Product",
+          press: () {},
+        ),
+        SizedBox(
+          height: getProportionateScreenWidth(20),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...List.generate(
+                demoProducts.length,
+                (index) => ProductCard(product: demoProducts[index]),
+              ),
+              SizedBox(
+                width: getProportionateScreenWidth(20),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
