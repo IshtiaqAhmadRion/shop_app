926f376ec4c636e67866fe74c1244347c7468f2c
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-18 22:16:28 +0600

    demo product

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/product.dart';
 import 'package:shop_app/screens/home/components/section_title.dart';
 
 import 'package:shop_app/size_config.dart';
@@ -37,6 +39,41 @@ class Body extends StatelessWidget {
             height: getProportionateScreenWidth(30),
           ),
           SectionTitle(text: "Populer Product", press: () {}),
+          SizedBox(
+            width: getProportionateScreenWidth(140),
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.02,
+                  child: Container(
+                    padding: EdgeInsets.all(getProportionateScreenWidth(20)),
+                    decoration: BoxDecoration(
+                      color: kSecondaryColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Image.asset(demoProducts[0].images[0]),
+                  ),
+                ),
+                const SizedBox(height: 5,),
+                Text(
+                  demoProducts[0].title,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  maxLines: 2,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "\$${demoProducts[0].price}",
+                      style: TextStyle(fontSize: getProportionateScreenWidth(18),fontWeight: FontWeight.w600,color: kPrimaryColor),
+                      
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     ));
