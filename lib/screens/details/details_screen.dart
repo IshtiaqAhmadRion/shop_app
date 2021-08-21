c02564b76cb2a2504efac502d2385af1bf09d087
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-21 20:02:50 +0600

    preferred size appbar

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/components/rounded_icon_btn.dart';
 import 'package:shop_app/models/product.dart';
+import 'package:shop_app/size_config.dart';
+
 class DetailsScreen extends StatelessWidget {
   static String routeName = "/details";
   const DetailsScreen({Key? key}) : super(key: key);
@@ -7,18 +10,37 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F6F9),
-      
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(AppBar().preferredSize.height),
+        child: AppbarContent(),
+      ),
     );
   }
 }
 
-// user PC
+class AppbarContent extends StatelessWidget {
+  const AppbarContent({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20),
+              ),
+              child: Row(
+                children: [
+                  RoundedIconBtn(iconData: Icons.arrow_back_ios, press: ()=>Navigator.pop(context),
+                  ),
+                ],
+              ),),
 
+    );
+  }
+}
 
 // class CustomAppBar extends PreferredSize {
 
-  
 //   @override
 //   Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
 //   @override
@@ -32,5 +54,3 @@ class ProductDetailsArguments {
   final Product product;
   ProductDetailsArguments({required this.product});
 }
-
-
