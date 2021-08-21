2e916100e93eead7e01b855509fb4cfa933f26f6
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-21 23:18:30 +0600

    argument error

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+class Body extends StatelessWidget {
+  final Product product;
+  const Body({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,55 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/components/rounded_icon_btn.dart';
+
 import 'package:shop_app/models/product.dart';
-import 'package:shop_app/size_config.dart';
+import 'package:shop_app/screens/details/components/body.dart';
+
+import 'components/custom_app_bar.dart';
 
 class DetailsScreen extends StatelessWidget {
   static String routeName = "/details";
   const DetailsScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final Object? args =
+        ModalRoute.of(context)!.settings.arguments;
     return Scaffold(
       backgroundColor: Color(0xFFF5F6F9),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(AppBar().preferredSize.height),
-        child: AppbarContent(),
+      appBar: preferredAppBar(),
+      body: Body(
+        product: args!.product,
       ),
     );
   }
 }
 
-class AppbarContent extends StatelessWidget {
-  const AppbarContent({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: getProportionateScreenWidth(20),
-              ),
-              child: Row(
-                children: [
-                  RoundedIconBtn(iconData: Icons.arrow_back_ios, press: ()=>Navigator.pop(context),
-                  ),
-                ],
-              ),),
-
-    );
-  }
-}
-
-// class CustomAppBar extends PreferredSize {
-
-//   @override
-//   Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [],
-//     );
-//   }
-// }
 class ProductDetailsArguments {
   final Product product;
   ProductDetailsArguments({required this.product});
