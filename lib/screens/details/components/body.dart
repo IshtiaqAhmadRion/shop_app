80816429ded9b29bea2ae13ac7c4b88f447893bf
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-22 17:21:33 +0600

    color dots

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:shop_app/constants.dart';
 
 import 'package:shop_app/models/product.dart';
-
+import 'package:shop_app/size_config.dart';
 
 import 'product_description.dart';
 import 'product_images.dart';
@@ -21,13 +21,14 @@ class Body extends StatelessWidget {
           color: Colors.white,
           child: Column(
             children: [
-              ProductDescription(product: product, pressOnSeeMore: (){},
+              ProductDescription(
+                product: product,
+                pressOnSeeMore: () {},
               ),
-              TopRoundedContainer(color: Color(0xFFF6F7F9), child: Row(
-                children: [],
+              TopRoundedContainer(
+                color: Color(0xFFF6F7F9),
+                child: ColorDots(product: product),
               ),
-              )
-
             ],
           ),
         ),
@@ -36,3 +37,59 @@ class Body extends StatelessWidget {
   }
 }
 
+class ColorDots extends StatelessWidget {
+  const ColorDots({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(20),
+      ),
+      child: Row(
+        children: [
+          ...List.generate(
+            product.colors.length,
+            (index) => ColorDot(color: product.colors[index]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  const ColorDot({
+    Key? key,
+    required this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final Color color;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 2),
+      padding: EdgeInsets.all(8),
+      height: getProportionateScreenWidth(40),
+      width: getProportionateScreenWidth(40),
+      decoration: BoxDecoration(
+          // color: product.colors[0],
+          shape: BoxShape.circle,
+          border: Border.all(color: isSelected ? kPrimaryColor : Colors.transparent)),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
