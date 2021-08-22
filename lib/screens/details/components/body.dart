554a1039fe7e1c067b31dddb2fc20b8ba7256125
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-08-22 14:33:01 +0600

    product images

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:shop_app/models/product.dart';
 import 'package:shop_app/size_config.dart';
+
+import 'product_images.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -10,14 +13,43 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          width: getProportionateScreenWidth(238),
-          child: AspectRatio(aspectRatio: 1,
-          child: Image.asset(product.images[0]),
+        ProductImages(product: product),
+        TopRoundedContainer(
+          color: Colors.white,
+          child: Column(
+            children: [],
           ),
         ),
-
       ],
+    );
+  }
+}
+
+class TopRoundedContainer extends StatelessWidget {
+  const TopRoundedContainer({
+    Key? key,
+    required this.color,
+    required this.child,
+  }) : super(key: key);
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: getProportionateScreenWidth(20)),
+      padding: EdgeInsets.only(top: getProportionateScreenWidth(20)),
+      width: double.infinity,
+      height: 250,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: child,
     );
   }
 }
---
+++ lib/screens/details/components/product_images.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+
+class ProductImages extends StatefulWidget {
+  const ProductImages({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  _ProductImagesState createState() => _ProductImagesState();
+}
+
+class _ProductImagesState extends State<ProductImages> {
+  int selectedItem = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: getProportionateScreenWidth(238),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(widget.product.images[selectedItem]),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ...List.generate(
+              widget.product.images.length,
+              (index) => buildSmallPreview(index),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  GestureDetector buildSmallPreview(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedItem = index;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: getProportionateScreenWidth(15)),
+        padding: EdgeInsets.all(getProportionateScreenHeight(8)),
+        height: getProportionateScreenWidth(48),
+        width: getProportionateScreenWidth(48),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color:
+                  selectedItem == index ? kPrimaryColor : Colors.transparent),
+        ),
+        child: Image.asset(widget.product.images[index]),
+      ),
+    );
+  }
+}
